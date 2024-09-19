7ec1ffbc90b69097243336696fbf1fcc4c2dfc66
hitiuees <ghbadr132800@gmail.com>
2024-09-19 18:36:21 +0100

    initial commit+user feautures

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,7 +6,7 @@ const router =express.Router()
 router.post('/register',async(req,res)=>{
     if (!req.body || !req.body.firstname || !req.body.lastname || !req.body.email || !req.body.password) {
         return res.status(400).send( { error: "Missing data", statuscode: 400 } );
-    }
+    } 
     
     const {firstname,lastname,email,password}=req.body
 const result=await register({firstname,lastname,email,password})
